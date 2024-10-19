87b67c996fd8f8bd339d419c946a5bd66b095fdc
Thomas Marks <marksta@umich.edu>
2024-10-19 17:56:35 -0400

    comments

--- py/html.py
+++ py/html.py
@@ -1,10 +1,5 @@
 from enum import Enum
 
-SELF_CLOSING_TAGS = [
-    "area", "base", "br", "col", "embed", "hr", "img", "input",
-    "link", "meta", "param", "source", "track", "wbr",
-]
-
 class Text:
     def __init__(self, text, parent):
         self.text = text
@@ -24,17 +19,65 @@ class Element:
     def __repr__(self):
         return "<" + self.tag + ">"
 
+HTMLParseState = Enum('HTMLParseState', [
+    'InText', "InTag", "InEntity", "InComment",
+])
+
 class HTMLParser:
+    SELF_CLOSING_TAGS = [
+        "area", "base", "br", "col", "embed", "hr", "img", "input",
+        "link", "meta", "param", "source", "track", "wbr",
+    ]
+
+    HEAD_TAGS = [
+        "base", "basefont", "bgsound", "noscript",
+        "link", "meta", "title", "style", "script",
+    ]
+
     def __init__(self, body):
         self.body = body
         self.unfinished = []
         self.out = []
+        self.pos = 0
+
+    def peek(self, n = 0):
+        if self.pos + n >= len(self.body):
+            return '\0'
+        else:
+            return self.body[self.pos + n]
+
+    def advance(self, n = 1):
+        self.pos += n
+        return self.peek(n)
+
+    def starts_comment(self):
+        if self.peek() == '<' and self.peek(1) == '!' and \
+           self.peek(2) == '-' and self.peek(3) == '-':
+            return True
+        return False
+
+    def ends_comment(self):
+        if self.peek() == '-':
+            if self.peek(1) == '-':
+                if self.peek(2) == '>':
+                    return True
+        return False
 
     def parse(self):
         state = HTMLParseState.InText
         text = ""
         buf = ""
-        for c in self.body:
+        comment_found = 0
+        i = 0;
+        c = self.peek()
+        while c != '\0':
+            if self.starts_comment():
+                c = self.advance(2)
+                while c != '\0' and not self.ends_comment():
+                    c = self.advance()
+                if c == '\0':
+                    break
+
             if state == HTMLParseState.InText:
                 if c == "<":
                     state = HTMLParseState.InTag
@@ -61,6 +104,8 @@ class HTMLParser:
                 else:
                     buf += c
 
+            c = self.advance()
+
         if state == HTMLParseState.InText and text:
             self.add_text(text)
 
@@ -81,6 +126,7 @@ class HTMLParser:
 
     def add_text(self, text):
         if text.isspace() or not text: return
+        self.implicit_tags(None)
         parent = self.unfinished[-1]
         node = Text(text, parent)
         parent.children.append(node)
@@ -88,14 +134,16 @@ class HTMLParser:
     def add_tag(self, tag):
         tag, attributes = self.get_attributes(tag)
         if tag.startswith("!"): return
-        if tag in SELF_CLOSING_TAGS:
-            parent = self.unfinished[-1]
-            node = Element(tag, attributes, parent)
-            parent.children.append(node)
-        elif tag.startswith("/"):
+        self.implicit_tags(tag)
+
+        if tag.startswith("/"):
             if len(self.unfinished) == 1: return
             node = self.unfinished.pop()
             parent = self.unfinished[-1]
+            parent.children.append(node)
+        elif tag in self.SELF_CLOSING_TAGS:
+            parent = self.unfinished[-1]
+            node = Element(tag, attributes, parent)
             parent.children.append(node)
         else:
             parent = self.unfinished[-1] if self.unfinished else None
@@ -103,15 +151,32 @@ class HTMLParser:
             self.unfinished.append(node)
 
     def finish(self):
+        if not self.unfinished:
+            self.implicit_tags(None)
         while len(self.unfinished) > 1:
             node = self.unfinished.pop()
             parent = self.unfinished[-1]
             parent.children.append(node)
         return self.unfinished.pop()
 
-HTMLParseState = Enum('HTMLParseState', [
-    'InText', "InTag", "InEntity"
-])
+    def implicit_tags(self, tag):
+        while True:
+            open_tags = [node.tag for node in self.unfinished]
+            if open_tags == [] and tag != "html":
+                self.add_tag("html")
+            elif open_tags == ["html"] \
+                 and tag not in ["head", "body", "/html"]:
+                if tag in self.HEAD_TAGS:
+                    self.add_tag("head")
+                else:
+                    self.add_tag("body")
+            elif open_tags == ["html", "head"] and \
+                 tag not in ["/head"] + self.HEAD_TAGS:
+                self.add_tag("/head")
+            else:
+                break
+
+
 
 def entity(buf):
     if buf == "lt":
