6af173134d3d35785be9f4d188b858b84793b47c
Thomas Marks <marksta@umich.edu>
2024-10-19 15:12:41 -0400

    move layout and constants to new files

--- py/browser.py
+++ py/browser.py
@@ -1,94 +1,11 @@
 import tkinter
 import tkinter.font
-from url import URL
-from html import lex, Text, Tag, print_tokens
 import platform
 import os
-
-WIDTH, HEIGHT = 800, 600
-SCROLL_STEP = 100
-SCROLL_SPEED = 2
-HSTEP, VSTEP = 13, 18
-ADDRESSBAR_HEIGHT = 1.25 * VSTEP
-SCROLLBAR_WIDTH = HSTEP
-SCROLLBAR_OFFSET = 5
-
-FONTS = {}
-
-def get_font(size, weight, style):
-    key = (size, weight, style)
-    if key not in FONTS:
-        font = tkinter.font.Font(size = size, weight = weight, slant = style)
-        label = tkinter.Label(font=font)
-        FONTS[key] = (font, label)
-    return FONTS[key][0]
-
-class Layout:
-    def __init__(self, tokens, width):
-        self.cursor_x = HSTEP
-        self.cursor_y = VSTEP + ADDRESSBAR_HEIGHT
-        self.width = width
-        self.size = 16
-        self.weight = "normal"
-        self.style = "roman"
-        self.line = []
-        self.display_list = []
-
-        for tok in tokens:
-            self.token(tok)
-
-        self.flush()
-
-    def token(self, tok):
-        if isinstance(tok, Text):
-            for word in tok.text.split():
-                self.word(word)
-        elif tok.tag == "i":
-            self.style = "italic"
-        elif tok.tag == "/i":
-            self.style = "roman"
-        elif tok.tag == "b":
-            self.weight = "bold"
-        elif tok.tag == "/b":
-            self.weight = "normal"
-        elif tok.tag == "small":
-            self.size -= 2
-        elif tok.tag == "/small":
-            self.size += 2
-        elif tok.tag == "big":
-            self.size += 4
-        elif tok.tag == "/big":
-            self.size -= 4
-        elif tok.tag == "br" or tok.tag == "/br":
-            self.flush()
-        elif tok.tag == "/p":
-            self.flush()
-            self.cursor_y += VSTEP
-
-    def word(self, word):
-        font =  get_font(self.size, self.weight, self.style)
-        w = font.measure(word)
-        if self.cursor_x + w > self.width - HSTEP:
-            self.flush()
-
-        self.line.append((self.cursor_x, word, font))
-        self.cursor_x += w + font.measure(" ")
-
-    def flush(self):
-        if not self.line: return
-        metrics = [font.metrics() for x, word, font in self.line]
-        max_ascent = max([metric["ascent"] for metric in metrics])
-        baseline = self.cursor_y + 1.25 * max_ascent
-
-        for x, word, font in self.line:
-            y = baseline - font.metrics("ascent")
-            self.display_list.append((x, y, word, font))
-
-        max_descent = max([metric["descent"] for metric in metrics])
-        self.cursor_y = baseline + 1.25 * max_descent
-
-        self.cursor_x = HSTEP
-        self.line = []
+from constants import *
+from url import URL
+from html import lex
+from layout import Layout
 
 class Browser:
     def __init__(self):
